acbe8fc7475d4d19c62749d5cfafb9d765d6d0a5
Eric Richter <richteer@lastprime.net>
2018-07-09 18:30:03 -0500

    signups: sort by react time, ping raider role on post (WIP

--- signups.py
+++ signups.py
@@ -1,4 +1,6 @@
 import asyncio
+import discord
+from datetime import datetime
 from emoji import emojize as emojiA
 
 def emoji(s):
@@ -32,38 +34,42 @@ class SignupPost():
 		await self.client.edit_message(self.post, self.generate_message())
 
 	def attend(self, user):
-		self.responses[str(user)] = (user, self.ATTEND)
+		self.responses[str(user)] = (user, self.ATTEND, datetime.now())
 	def bail(self, user):
-		self.responses[str(user)] = (user, self.BAIL)
+		self.responses[str(user)] = (user, self.BAIL, datetime.now())
 	def maybe(self, user):
-		self.responses[str(user)] = (user, self.MAYBE)
+		self.responses[str(user)] = (user, self.MAYBE, datetime.now())
 	def late(self, user):
-		self.responses[str(user)] = (user, self.LATE)
+		self.responses[str(user)] = (user, self.LATE, datetime.now())
 
 	def generate_message(self):
-		attend = [u[0].name for u in self.responses.values() if u[1] == SignupPost.ATTEND]
-		bail   = [u[0].name for u in self.responses.values() if u[1] == SignupPost.BAIL]
-		maybe  = [u[0].name for u in self.responses.values() if u[1] == SignupPost.MAYBE]
-		late   = [u[0].name for u in self.responses.values() if u[1] == SignupPost.LATE]
+		tmp = sorted(self.responses.values(), key=lambda x: x[2])
+		# Sort by the local timestamp of when this person reacted (always take last one)
+		attend = [u[0].name for u in tmp if u[1] == SignupPost.ATTEND]
+		bail   = [u[0].name for u in tmp if u[1] == SignupPost.BAIL]
+		maybe  = [u[0].name for u in tmp if u[1] == SignupPost.MAYBE]
+		late   = [u[0].name for u in tmp if u[1] == SignupPost.LATE]
+
+		role = discord.utils.get(self.channel.server.roles, name="Raider")
 
 		ret = '''
-{}
+{0}
 
-Please react with one of the following:
+{1} Please react with one of the following:
 :white_check_mark: - I can make it
 :x: - I cannot make it
 :question: - Maybe
 :clock10: - Going to be late
-'''.format(self.text)
+'''.format(self.text, role.mention)
 
 		if attend:
 			ret += "\nAttending:\n{}\n".format("\n".join(attend))
-		if bail:
-			ret += "\nBailing\n{}\n".format("\n".join(bail))
 		if maybe:
-			ret += "\nMaybe\n{}\n".format("\n".join(maybe))
+			ret += "\nMaybe:\n{}\n".format("\n".join(maybe))
 		if late:
-			ret += "\nLate\n{}\n".format("\n".join(late))
+			ret += "\nLate:\n{}\n".format("\n".join(late))
+		if bail:
+			ret += "\nUnavailable:\n{}\n".format("\n".join(bail))
 
 		return ret
 
